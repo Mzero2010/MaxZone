455d91ce53f168a67e01846957fa8a9e9db00844
Mzero2010 <renepc2010@gmail.com>
2017-01-03 09:17:04 -0600

    Repo
    Version 1.0.3

--- plugin.video.Mzero/channels/Animeid Io.py
+++ plugin.video.Mzero/channels/Animeid Io.py
@@ -55,8 +55,8 @@ def letras(item):
     data = scrapertools.anti_cloudflare(item.url, headers=CHANNEL_DEFAULT_HEADERS, host=CHANNEL_HOST)
     data = re.sub(r"\n|\r|\t|\s{2}|&nbsp;|<Br>|<BR>|<br>|<br/>|<br />|-\s", "", data)
 
-    data = scrapertools.get_match(data, '<div class="list_search">(.+?)</div>')
-    patron = '<a href="([^"]+)[^>]+>([^<]+)</a>'
+    data = scrapertools.get_match(data, '<div class="list_search"><ul>(.+?)</div>')
+    patron = '<li class="first-char"><a href="([^"]+)[^>]+>([^<]+)</a></li>'
     matches = re.compile(patron, re.DOTALL).findall(data)
 
     for scrapedurl, scrapedtitle in matches:
@@ -79,8 +79,9 @@ def generos(item):
     data = scrapertools.anti_cloudflare(item.url, headers=CHANNEL_DEFAULT_HEADERS, host=CHANNEL_HOST)
     data = re.sub(r"\n|\r|\t|\s{2}|&nbsp;|<Br>|<BR>|<br>|<br/>|<br />|-\s", "", data)
 
-    data = scrapertools.get_match(data, '<div class="recent"><nav class="menu_series genre right">(.*?)</div>')
-    patron = '<a href="([^"]+)[^>]+>([^<]+)</a>'
+    data = scrapertools.get_match(data, '<div class="recent"><nav class="menu_series genre right"><ul>(.*?)</div>')
+    patron = '<li  class="even" ><a href="([^"]+)[^>]+>([^<]+)</a></li>'
+	patron += '<li  class="oven" ><a href="([^"]+)[^>]+>([^<]+)</a></li>'
     matches = re.compile(patron, re.DOTALL).findall(data)
 
     for scrapedurl, scrapedtitle in matches:
@@ -119,6 +120,11 @@ def series(item):
 
     data = re.sub(r"\n|\r|\t|\s{2}|&nbsp;|<Br>|<BR>|<br>|<br/>|<br />|-\s", "", data)
 
+	patron = '<div class="anime_list_body"><ul class="listing"[^<]+'
+    patron += ''<a href="([^"]+)"[^<]+<img src="[^"]+"
+    patron += '<span[^<]+</span[^<]+'
+    patron += '<a[^>]+>([^<]+)</a.*?'
+    patron += '<p class="sumer"><span>Línea de historia: </span>(.*?)</p'
     patron = "<div class=.+?<img src=\"([^\"]+)\".+?<a.+?href=\"([^\"]+)\">(.*?)</a><p>(.*?)</p>"
     matches = re.compile(patron, re.DOTALL).findall(data)
     itemlist = []
@@ -157,9 +163,9 @@ def episodios(item):
     data = re.sub(r"\n|\r|\t|\s{2}|&nbsp;|<Br>|<BR>|<br>|<br/>|<br />|-\s", "", data)
 
     patron = "<p><span>(.*?)</span>"
-    aux_plot = scrapertools.find_single_match(data, patron)
+    aux_plot = scrapertools.find_single_match(data, '<div class="anime_info_episodes">.*?</div>(.*?)</ul>')
 
-    patron = '<li><ahref="([^"]+)">(.*?)</a></li><li>(.*?)</li>'
+    patron = '<li><a href="([^"]+)">([^<]+)</a></li>'
     matches = re.compile(patron, re.DOTALL).findall(data)
 
     pelicula = False
